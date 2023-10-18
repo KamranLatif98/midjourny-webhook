09ccd8d4dc807287f844bdabb789e2207c368c15
iqbalazeem80 <iqbal.teczon@gmail.com>
2023-10-18 18:11:32 +0500

    First Commit

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ app.get("/webhook", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-  console.log("Received Webhook:", req.body);
-  webhookData = req.body; // Save the posted data
-  console.log("Updated webhookData:", webhookData);
+  webhookData = req.body;
+
   res.status(200).send(webhookData);
 });
 const PORT = process.env.PORT || 3000;
